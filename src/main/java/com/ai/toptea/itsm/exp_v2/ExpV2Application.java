f5231609ee56164e8112f176814602966651725a
zhongzh7 <zhongzh7@asiainfo.com>
2025-11-26 11:41:20 +0800

    0

---
+++ src/main/java/com/ai/toptea/itsm/exp_v2/ExpV2Application.java
@@ -0,0 +1,13 @@
+package com.ai.toptea.itsm.exp_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpV2Application.class, args);
+    }
+
+}
